e30979482addf796177da25cbabfae36f0432287
Erling Tokheim <e@tokheim.no>
2016-12-05 16:04:10 +0100

    PERFORMANCE: Avoided FSL by updating updatePositions()
    Performance:
    	- Avoided FSL in for loop by moving the function that caused layout outside the loop
    	- Added requestAnimationFrame to function making visual change

--- js/main.js
+++ js/main.js
@@ -503,9 +503,16 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  var scrollTop = document.body.scrollTop;
+  var phase;
+
+  requestAnimationFrame(updatePositionsStyling);
+
+  function updatePositionsStyling() {
+      for (var i = 0; i < items.length; i++) {
+      phase = Math.sin((scrollTop / 1250) + (i % 5));
+      items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    }
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
